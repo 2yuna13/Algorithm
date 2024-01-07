d6cd8ac99d852cd914931766ce0ad22d1186d702
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-01-07 18:18:52 +0900

    [Silver IV] Title: 수 찾기, Time: 168 ms, Memory: 50396 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,14 @@
+n = int(input())
+a = set(map(int, input().split()))
+
+m = int(input())
+targets = list(map(int, input().split()))
+
+res = []
+for num in targets:
+    if num in a:
+        res.append(1)
+    else:
+        res.append(0)
+
+print(*res, sep='\n')
