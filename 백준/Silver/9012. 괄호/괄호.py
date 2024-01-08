918bcabbfd5038db6e8ae2cf1c9b5d9d522235ae
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-01-08 23:50:19 +0900

    [Silver IV] Title: 괄호, Time: 48 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,24 @@
+T = int(input())
+
+res = []
+
+for i in range(T):
+    str = input()
+    myStack = []
+    chk = 0 #chk == 0 YES / 1 No
+    for j in range(len(str)):
+        if str[j] == '(' :
+            myStack.append(str[j])
+        else: 
+            if len(myStack) == 0:
+                chk = 1
+                break
+            else:
+                myStack.pop()
+            
+    if chk == 1 or len(myStack) != 0:
+        res.append("NO")
+    else:
+        res.append("YES")
+
+print(*res, sep="\n")
