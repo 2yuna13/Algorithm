1f510220f5c0c0eb6d30b7c68f790a8d1aec1a68
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-01-16 10:39:49 +0900

    [Silver I] Title: 나이트의 이동, Time: 1324 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -9,26 +9,26 @@ dy = [-2, -1, 1, 2, 2, 1, -1, -2]
 
 def bfs(x, y):
     q = deque([(x, y)])
-    graph[y][x] = 0
+    visited = [[0] * l for _ in range(l)]
+    visited[y][x] = 1
 
     while q:
         x, y = q.popleft()
 
         if x == target[0] and y == target[1]:
-            return graph[y][x]
+            return visited[y][x] - 1
 
         for i in range(8):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < l and 0 <= ny < l and graph[ny][nx] == 0:
+            if 0 <= nx < l and 0 <= ny < l and visited[ny][nx] == 0:
                 q.append([nx, ny])
-                graph[ny][nx] = graph[y][x] + 1
+                visited[ny][nx] = visited[y][x] + 1
 
 
 t = int(input())
 for _ in range(t):
     l = int(input())
-    graph = [[0] * l for _ in range(l)]
-    knight = list(map(int, input().split()))
+    start = list(map(int, input().split()))
     target = list(map(int, input().split()))
-    print(bfs(knight[0], knight[1]))
+    print(bfs(start[0], start[1]))
