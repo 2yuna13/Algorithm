54ba5d9d53231535c445919e457b7371a090f176
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-01-23 11:18:47 +0900

    [Gold IV] Title: 도서관, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.py
+++ 백준/Gold/1461. 도서관/도서관.py
@@ -3,32 +3,27 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 books = list(map(int, input().split()))
-books.sort()
+
+left = []
+right = []
+for book in books:
+    if book < 0:
+        left.append(book)
+    else:
+        right.append(book)
 
 cnt = []
-books_m = []
-books_p = []
+left.sort()
+right.sort()
 
-for i in books:
-    if i < 0:
-        books_m.append(i)
-    else:
-        books_p.append(i)
+for i in range(0, len(left), m):
+    cnt.append(abs(left[i]))
 
-for i in range(0, len(books_m), m):
-    cnt.append(books_m[i])
+for i in range(len(right)-1, -1, -m):
+    cnt.append(abs(right[i]))
 
-for i in range(len(books_p)-1, -1, -m):
-    cnt.append(books_p[i])
-
-cnt = [abs(i) for i in cnt]
+cnt.sort()
 max_cnt = max(cnt)
+total_cnt = sum(cnt) * 2 - max_cnt
 
-for i in range(len(cnt)):
-    if cnt[i] == max_cnt:
-        cnt[i] = cnt[i]
-
-    else:
-        cnt[i] = cnt[i] * 2
-
-print(sum(cnt))
+print(total_cnt)
