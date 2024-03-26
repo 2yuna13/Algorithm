361b55cbd78d4043eb98af0d10fa80094e9aaa10
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-03-26 09:34:03 +0900

    [Silver IV] Title: 차집합, Time: 980 ms, Memory: 146304 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.py
+++ 백준/Silver/1822. 차집합/차집합.py
@@ -2,7 +2,12 @@ N, M = map(int, input().split())
 
 A = set(map(int, input().split()))
 B = set(map(int, input().split()))
-C = A - B
+C = []
 
+for i in A:
+    if i not in B:
+        C.append(i)
+
+C.sort()
 print(len(C))
-print(*(sorted(C)))
+print(*C)
