5769654aa55e0bc0eb0c5df0028b8ab09ac03f98
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-04-17 18:13:46 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,40 +1,27 @@
 def solution(a, b, c, d):    
-    # 네 주사위가 p로 같으면
-    if a == b == c == d:
+    nums = [a, b, c, d]
+    # 각 숫자가 리스트에 몇개 있는지 확인
+    counts = [nums.count(i) for i in nums]
+    
+    # 네 주사위에서 나온 숫자가 모두 같을 때
+    if max(counts) == 4:
         return 1111 * a
-    
-    # 세 주사위가 p로 같고, 나머지 하나가 q 
-    elif a == b == c:
-        return (10 * a + d) ** 2
-    elif a == b == d:
-        return (10 * a + c) ** 2
-    elif a == c == d:
-        return (10 * a + b) ** 2
-    elif b == c == d:
-        return (10 * b + a) ** 2
-    
-    # 두 개씩 같은 값, 각 p, q로 하면
-    elif a == b and c == d:
-        return (a + c) * abs(a - c)
-    elif a == c and b == d:
-        return (a + b) * abs(a - b)
-    elif a == d and b == c:
-        return (a + c) * abs(a - c)
-    
-    # 두 주사위가 p로 같고, 나머지 각 q, r이라 하면
-    elif a == b:
-        return c * d
-    elif a == c:
-        return b * d
-    elif a == d:
-        return b * c
-    elif b == c:
-        return a * d
-    elif b == d:
-        return a * c
-    elif d == c:
-        return a * b
-
-    # 모두 다르면
+    # 세 주사위에서 나온 숫자가 같을 때
+    elif max(counts) == 3:
+        p = nums[counts.index(3)]
+        q = nums[counts.index(1)]
+        return (10 * p + q) ** 2
+    elif max(counts) == 2:
+        # 두 개씩 같은 값
+        if min(counts) == 2:
+            return (a + c) * abs(a - c) if a == b else (a + b) * abs(a - b)
+        # 두 주사위 같고, 나머지 각각 다를 때
+        else:
+            p = nums[counts.index(2)]
+            return (a * b * c * d) / p ** 2
     else:
-        return min(a, b, c, d)
+        return min(nums)
+            
+            
+    
+        
