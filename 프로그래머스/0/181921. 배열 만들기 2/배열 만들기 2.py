0c4156584accab8f7ac23e62fedd87a299751cc1
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-04-19 14:29:43 +0900

    [level 0] Title: 배열 만들기 2, Time: 21.77 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -2,7 +2,10 @@ def solution(l, r):
     answer = []
     
     for i in range(l, r + 1):
-        if not set(str(i)) - set(['0', '5']):
+        if all(num in ['0', '5'] for num in str(i)):
             answer.append(i)
     
-    return answer if answer else [-1]
+    if len(answer) == 0:
+        answer.append(-1)
+        
+    return answer
