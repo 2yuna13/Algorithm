03a7301e08616d9d5a806f13bd3724f1311859ae
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-04-20 12:55:48 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
@@ -1,12 +1,10 @@
 def solution(arr, flag):
     answer = []
     
-    for i in range(len(arr)):
-         # flag[i]가 true라면 X의 뒤에 arr[i]를 arr[i] × 2 번 추가
-        if flag[i] == True:
-            answer.extend([arr[i]] * (arr[i] * 2))
-        # flag[i]가 false라면 X에서 마지막 arr[i]개의 원소를 제거
+    for i, j in zip(arr, flag):
+        if j:
+            answer += [i] * i * 2
         else:
-            answer = answer[:-arr[i]]
-            
+            answer = answer[:-i]
+
     return answer
