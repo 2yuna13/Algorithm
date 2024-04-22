2f7d3101604594dbe3421aad883b68bd7830bfc4
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-04-22 17:38:28 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,8 +1,6 @@
 def solution(arr, idx):
-    answer = -1
-    
-    for i, v in enumerate(arr):
-        if i + 1 > idx and v == 1:
-            return i  
+    for i in range(idx, len(arr)):
+        if arr[i] == 1:
+            return i
         
-    return answer
+    return -1
