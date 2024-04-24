7a11088f5556fe6777d6ab42705f62b8096cf203
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-04-24 15:36:41 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -0,0 +1,10 @@
+def solution(arr, delete_list):
+    answer = []
+    
+    for i in arr:
+        if i in delete_list:
+            continue
+        else:
+            answer.append(i)
+    
+    return answer
