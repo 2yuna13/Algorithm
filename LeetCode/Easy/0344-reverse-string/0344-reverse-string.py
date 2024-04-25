74cf0aaffa05e1c0fcf841992b7072d24aca1754
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-04-25 11:13:17 +0900

    [Easy] Time: 176 ms (24.72%), Memory: 20.9 MB (72.17%) - LeetHub

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.py
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.py
@@ -4,15 +4,6 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         
-        start = 0
-        end = len(s) - 1
-        
-        while start < end:
-            temp = s[start]
-            s[start] = s[end]
-            s[end] = temp
-            
-            start += 1
-            end -= 1
+        s.reverse()
             
             
