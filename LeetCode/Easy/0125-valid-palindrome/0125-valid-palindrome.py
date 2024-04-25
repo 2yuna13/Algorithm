b97a14853ea74b6340c07572b0fd8bdaff9faa08
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-04-25 09:34:08 +0900

    [Easy] Time: 51 ms (26.01%), Memory: 17.9 MB (26.56%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,8 +2,6 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         s_list = [i for i in s.lower() if i.isalnum()]
         
-        print(s_list)
-        
         if s_list == s_list[::-1]:
             return True
         else:
