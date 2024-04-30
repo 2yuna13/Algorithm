0bac6d5901d1e6f3d0f9894e0fb4ff65cb42d11c
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-04-30 13:57:15 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 84.06 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -12,45 +12,45 @@ def solution(rows, columns, queries):
     # 4. x2 에서 x1, (-1, 0)
     # x1, y1 => 회전하면서 rotate_list 저장
     for query in queries:
-        x1, y1, x2, y2 = query
+        x1, y1, x2, y2 = [q - 1 for q in query] 
         
         rotate_list = []        
-        temp = graph[x1 - 1][y1 - 1]
+        temp = graph[x1][y1]
         rotate_list.append(temp)
         
         # 1. y1 에서 y2 , (0, 1)
         for i in range(y1, y2):
-            cur = graph[x1 - 1][i]
+            cur = graph[x1][i]
             rotate_list.append(cur)
             
-            graph[x1 - 1][i] = temp
+            graph[x1][i] = temp
             temp = cur
             
         # 2. x1 에서 x2, (1, 0)
         for i in range(x1, x2):
-            cur = graph[i][y2 - 1]
+            cur = graph[i][y2]
             rotate_list.append(cur)
             
-            graph[i][y2 - 1] = temp
+            graph[i][y2] = temp
             temp = cur
         
         # 3. y2 에서 y1, (0, -1)
-        for i in range(y2 - 2, y1 - 2, -1):
-            cur = graph[x2 - 1][i]
+        for i in range(y2, y1, -1):
+            cur = graph[x2][i]
             rotate_list.append(cur)
             
-            graph[x2 - 1][i] = temp
+            graph[x2][i] = temp
             temp = cur
             
         # 4. x2 에서 x1, (-1, 0)
-        for i in range(x2 - 2, x1 - 1, -1):
-            cur = graph[i][y1 - 1]
+        for i in range(x2, x1, -1):
+            cur = graph[i][y1]
             rotate_list.append(cur)
             
-            graph[i][y1 - 1] = temp
+            graph[i][y1] = temp
             temp = cur
         
-        graph[x1 - 1][y1 - 1] = temp
+        graph[x1][y1] = temp
         
         answer.append(min(rotate_list))
         
