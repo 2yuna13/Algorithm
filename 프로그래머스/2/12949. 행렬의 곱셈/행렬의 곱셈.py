7e26a49f7d77e8bf44d975ba7918e55ee7b14bfb
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-05-03 10:22:25 +0900

    [level 2] Title: 행렬의 곱셈, Time: 9.01 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -0,0 +1,15 @@
+# 행렬 곱셈
+# arr1 행(r) * arr2 열(c)
+
+def solution(arr1, arr2):
+    r1, c1 = len(arr1), len(arr1[0])
+    r2, c2 = len(arr2), len(arr2[0])
+    
+    answer = [[0] * c2 for _ in range(r1)]
+    
+    for i in range(r1):
+        for j in range(c2):
+            for k in range(c1):
+                answer[i][j] += arr1[i][k] * arr2[k][j]
+
+    return answer
