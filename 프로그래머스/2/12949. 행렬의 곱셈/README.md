b13e898e62f6c6a9a51dd0c5b0cba1929a28625a
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-05-03 10:45:07 +0900

    [level 2] Title: 행렬의 곱셈, Time: 9.63 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949#) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 9.01 ms
+메모리: 10.6 MB, 시간: 9.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 03일 10:22:24
+2024년 05월 03일 10:45:07
 
 ### 문제 설명
 
--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -2,14 +2,15 @@
 # arr1 행(r) * arr2 열(c)
 
 def solution(arr1, arr2):
-    r1, c1 = len(arr1), len(arr1[0])
-    r2, c2 = len(arr2), len(arr2[0])
+    r1 = len(arr1)
+    r2 = len(arr2)
+    c2 = len(arr2[0])
     
     answer = [[0] * c2 for _ in range(r1)]
     
-    for i in range(r1):
-        for j in range(c2):
-            for k in range(c1):
+    for i in range(r1): # 결과 행 인덱스
+        for j in range(c2): # 결과 열 인덱스
+            for k in range(r2): # 곱할 때.. 
                 answer[i][j] += arr1[i][k] * arr2[k][j]
 
     return answer
