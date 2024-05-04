811cf25f4bb05bb8c25db88ff424c4ad36c97647
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-05-04 22:26:14 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.29 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -3,15 +3,12 @@ def solution(numer1, denom1, numer2, denom2):
     numer = numer1 * denom2 + numer2 * denom1
     denom = denom1 * denom2
     
-    divisor = []
+    n = 0
     
-    for i in range(1, numer + 1):
-        if numer % i == 0:
-            for j in range(1, denom + 1):
-                if denom % j == 0 and i == j:
-                    divisor.append(i)
-
-    n = divisor[-1]
+    for i in range(min(numer, denom), 0, -1):
+        if numer % i == 0 and denom % i == 0:
+            n = i
+            break
     
     if n != 0:
         numer //= n
