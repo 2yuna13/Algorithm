2b30fe47170b6de3f26bfdc095f3d5b849ee0c48
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-05-04 23:36:50 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.py
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.py
@@ -1,8 +1,3 @@
 def solution(n, k):
-    answer = 12000 * n + 2000 * k
     
-    while n >= 10:
-        answer -= 2000
-        n -= 10
-    
-    return answer
+    return 12000 * n + 2000 * (k - n // 10)
