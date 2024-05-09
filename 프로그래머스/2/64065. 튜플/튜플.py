78ba3052b993d1232c12ecaddd99d8b3285dfd22
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-05-09 14:48:21 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -2,7 +2,7 @@ def solution(s):
     answer = []
     s = s.strip("{}")
     s_list = s.split("},{")
-    s_list = sorted(s_list, key=lambda x: len(x))
+    s_list.sort(key=lambda x: len(x))
     
     for i in s_list:
         i = list(map(int, i.split(",")))
