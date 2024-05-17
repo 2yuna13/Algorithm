8ac75dd43b1da1d039c34f57ba9aa37527cd0a81
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-05-17 09:47:47 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,22 +1,16 @@
 # n -> 100,000,000 이하 / n, logn
-
-# 3진법으로 변환 
-# 3진법 뒤집기 
-# 10진법으로 표현 (변환한거 이용하기..?)
-
-# 진법 변환         
+  
 def solution(n):
     answer = 0
     
-    if n == 0:
-        return 0
-    
+    # 3진법 변환
     nums = []
     while n:
         remainder = n % 3
         n //= 3
         nums.append(remainder)
-        
+    
+    # 뒤집고 10진법으로 표현
     for i, v in enumerate(nums[::-1]):
         if i == 0:
             answer += v
