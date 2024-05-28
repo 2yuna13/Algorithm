2132f51c904d5280823a351476a727db40582aeb
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-05-28 15:09:38 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,12 @@
-# 5,000 이하 / 2,000,000 이하 -> n / log n
-# 카펫 가로 >= 세로
+# 가로 > 세로
 # brown = (yellow 가로 * 2) + (yellow 세로 * 2) + 4
-def solution(brown, yellow):
+
+def solution(brown, yellow):    
     for i in range(1, yellow + 1):
         if yellow % i == 0:
             width = yellow // i
             height = i
             if brown == (width * 2) + (height * 2) + 4:
                 return [width + 2, height + 2]
+
+
