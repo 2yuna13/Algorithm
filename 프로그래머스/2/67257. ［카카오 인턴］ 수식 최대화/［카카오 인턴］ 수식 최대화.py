a56a8871452ef20d7018176576fa8f0bf8c39795
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-05-29 17:15:02 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.78 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -5,58 +5,27 @@
 import re
 from itertools import permutations
 
-def toPostFix(tokens, priority):
-    stack = [] # 연산자 스택
-    postfix = [] # 출력 배열(문자열)
-    
-    for token in tokens:
-        if token.isdigit(): postfix.append(token) # 숫자는 바로 추가
-        else:
-            if not stack: stack.append(token)
-            else:
-                while stack:
-                    if priority[token] <= priority[stack[-1]]:
-                        postfix.append(stack.pop())
-                    else: break
-                        
-                stack.append(token)
-                
-    while stack:
-        postfix.append(stack.pop())
-
-    return postfix
-
 def calc(tokens):
-    stack = []
-    
-    for token in tokens:
-        if token.isdigit():
-            stack.append(int(token))
-            continue
-        
-        num1 = stack.pop()
-        num2 = stack.pop()
-        
-        if token == '*':
-            stack.append(num2 * num1)
-        elif token == '+':
-            stack.append(num2 + num1)
-        else:
-            stack.append(num2 - num1)
-            
-    return stack.pop()
+    num1, exp, num2 = tokens
+    if exp == '*':
+        return int(num1) * int(num2)
+    elif exp == '+':
+        return int(num1) + int(num2)
+    else:
+        return int(num1) - int(num2)
             
 def solution(expression):
-    answer = 0
-    
-    # 숫자, 연산자 분리
-    tokens = re.split("([-*+])", expression)
+    answer = []
     operators = ['-', '*', '+']
     
-    # 연산자 우선순위 -> {'-': 0, '*': 1, '+': 2}, ..., {'+': 0, '*': 1, '-': 2}
-    for i in map(list, permutations(operators)):
-        priority = {o:p for p, o in enumerate(list(i))}
-        postfix = toPostFix(tokens, priority)
-        answer = max(answer, abs(calc(postfix)))
+    for operator in permutations(operators):
+        temp = re.split("([-*+])", expression)
+        for exp in operator:
+            while exp in temp:
+                idx = temp.index(exp)
+                temp[idx-1] = str(calc(temp[idx-1:idx+2]))
+                temp[idx] = temp[idx + 1] = ''
+                temp = [i for i in temp if i]
+        answer.append(abs(int(temp[0])))
         
-    return answer
+    return max(answer)
