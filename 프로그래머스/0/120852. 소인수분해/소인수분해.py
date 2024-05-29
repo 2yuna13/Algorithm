044206ae02224d9a25654f26409270c23849ac56
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-05-29 18:35:32 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -1,16 +1,13 @@
 def solution(n):
     answer = []
-    # 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-    # 2, 3, 
-    for i in range(2, n + 1):
-        # 2
-        cnt = 0
-        for j in range(2, i + 1):
-            if i % j == 0:
-                cnt += 1
-        
-        if cnt == 1:
-            if n % i == 0:
-                answer.append(i)
-                
+    d = 2
+    
+    while d <= n:
+        if n % d == 0:
+            n /= d
+            if d not in answer:
+                answer.append(d)
+        else:
+            d += 1   
+    
     return answer
