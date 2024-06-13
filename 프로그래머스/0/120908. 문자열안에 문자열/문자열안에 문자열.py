d2d8bf4b3f618d626b0d5dc51ab9e6a024743bf5
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-06-13 17:30:56 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -1,7 +1,6 @@
 def solution(str1, str2):  
     
-    for i in range(len(str1) - len(str2) + 1):
-        if str1[i : i + len(str2)] == str2:
-            return 1
+    if str2 in str1:
+        return 1
     
     return 2
