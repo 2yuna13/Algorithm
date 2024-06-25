eb8aaab7f333083409f8b1bee1eece79302f4442
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-06-25 23:43:03 +0900

    [level 0] Title: 안전지대, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -5,25 +5,30 @@ def solution(board):
     dx = [-1, 1, 0, 0, -1, -1, 1, 1]
     dy = [0, 0, -1, 1, -1, 1, -1, 1]
     
-    danger = []
+    danger = [[0] * n for _ in range(n)]
     
     for i in range(n):
         for j in range(n):
-            # 지뢰 위치 표시
+            # 지뢰 위치 표시 & 주변 8칸을 위험으로 표시
             if board[i][j] == 1:
-                danger.append((i,j))
+                danger[i][j] = 1
+                for k in range(8):
+                    nx = i + dx[k]
+                    ny = j + dy[k]
+                    if 0 <= nx < n and 0 <= ny < n:
+                        danger[nx][ny] = 1
 
     # 지뢰가 있는 위치를 포함하여 주변 8칸을 위험으로 표시
-    for x, y in danger:
-        for i in range(8):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n:
-                board[nx][ny] = 1
+    # for x, y in danger:
+    #     for i in range(8):
+    #         nx = x + dx[i]
+    #         ny = y + dy[i]
+    #         if 0 <= nx < n and 0 <= ny < n:
+    #             board[nx][ny] = 1
     
     for i in range(n):
         for j in range(n):
-            if board[i][j] == 0:
+            if danger[i][j] == 0:
                 answer += 1
                 
     return answer
