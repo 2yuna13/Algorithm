7323c7e64d30dcc908bff93066b891ce793f1187
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-07-10 00:53:28 +0900

    [level 0] Title: 평행, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -0,0 +1,14 @@
+# 두 점의 기울기 = 나머지 두 점의 기울기
+
+def solution(dots):
+    [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots
+    
+    def slope(x1, y1, x2, y2):
+        return (y2 - y1) / (x2 - x1)
+    
+    if slope(x1, y1, x2, y2) == slope(x3, y3, x4, y4):
+        return 1
+    if slope(x1, y1, x3, y3) == slope(x2, y2, x4, y4):
+        return 1
+    
+    return 0
