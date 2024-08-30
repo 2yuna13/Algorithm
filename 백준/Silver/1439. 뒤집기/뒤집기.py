18f24a37a51e10c02ad6beb8b188a82db0d7f3d8
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-08-30 12:09:44 +0900

    [Silver V] Title: 뒤집기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,20 +1,6 @@
 s = input()
-
-#0으로 바꾸는 경우
-count0 = 0
-#1로 바꾸는 경우
-count1 = 0
-
-if s[0] == '1':
-    count0 += 1
-else:
-    count1 += 1
-
-for i in range(len(s) -1):
-    if s[i] != s[i + 1]:
-        if s[i + 1] == '1':
-            count0 += 1
-        else:
-            count1 += 1
-
-print(min(count0, count1))
+count = 0
+for i in range(len(s)-1):
+    if s[i] != s[i+1]:
+        count += 1
+print((count + 1) // 2)
