9286eecf2b3030740448fdf48fdc6e63c1351f80
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-10-02 15:21:53 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,8 +1,21 @@
 def solution(s):
     # 딕셔너리 선언 (영단어 -> 숫자)
-    dic = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+    dic = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4',
+           'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
     
-    for word, num in dic.items():
-        s = s.replace(word, num)
+    answer = ''
+    temp = ''
+    
+    for i in s:
+        #숫자인 경우
+        if i.isdigit():
+            answer += i
+        #문자인 경우
+        else:
+            temp += i
+            #영단어에 해당하면 숫자로 변경
+            if temp in dic:
+                answer += dic[temp]
+                temp = ''
             
-    return int(s)
+    return int(answer)
