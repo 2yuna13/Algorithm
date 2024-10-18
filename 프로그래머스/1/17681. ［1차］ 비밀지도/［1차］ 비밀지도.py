61a128596dad9ee8fd2e6f034e9f84bf4de3f7f7
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-10-18 11:18:41 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,29 +1,13 @@
 def solution(n, arr1, arr2):
     answer = []
     
-    #이진법으로 변경
-    for i in range(len(arr1)):
-        arr1[i] = binaryChange(arr1[i], n)
+    for i in range(n):
+        # bin 이용해서 이진수 변환, 비트 OR 연산
+        bin_or = bin(arr1[i] | arr2[i])[2:]
+        # 길이를 n만큼 채워줌
+        bin_or = bin_or.zfill(n)
         
-    for j in range(len(arr2)):
-        arr2[j] = binaryChange(arr2[j], n)
-    
-    #두 배열 탐색하면서 # or 공백으로 표시
-    for i, j in zip(arr1, arr2):
-        res = ''
-        for x, y in zip(i, j):
-            if x == y == '0':
-                res += ' '
-            else:
-                res += '#'      
+        res = bin_or.replace('1', '#').replace('0', ' ')
         answer.append(res)
     
     return answer
-
-#이진법으로 변경
-def binaryChange(num, length):
-    nums = []
-    while num:
-        num, digit = divmod(num, 2)
-        nums.append(str(digit))
-    return ''.join(nums)[::-1].zfill(length)
