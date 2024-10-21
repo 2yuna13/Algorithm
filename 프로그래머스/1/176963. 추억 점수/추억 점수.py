4c77e001a0d73b2f497f20eaab3ae9d7aa09c7fb
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-10-21 18:09:19 +0900

    [level 1] Title: 추억 점수, Time: 0.96 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,10 +1,12 @@
 def solution(name, yearning, photo):
     answer = []
+    dictionary = dict(zip(name, yearning))
+    
     for i in photo:
         score = 0
         for j in i:
-            if j in name:
-                score += yearning[name.index(j)]
+            if j in dictionary:
+                score += dictionary[j]
         answer.append(score)
         
     return answer
