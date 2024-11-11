dd0a06d2ea3ccd93cefac79be0a5f6775e77c7d8
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-11-11 16:17:58 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -2,17 +2,18 @@ def solution(board, moves):
     answer = 0
     basket = []
     
-    for i in moves:
+    for move in moves:
         #바구니로 옮기고 캐릭터 위치 -> 0
-        for j in board:
-            if j[i - 1] != 0:
-                basket.append(j[i - 1])
-                j[i - 1] = 0
+        for row in board:
+            if row[move - 1] != 0:
+                basket.append(row[move - 1])
+                row[move - 1] = 0
                 break
         # 바구니에 쌓고 직전 캐릭터와 동일하면 pop, answer += 2
         if len(basket) > 1 and basket[-1] == basket[-2]:
-            basket.pop()
-            basket.pop()
+            # basket.pop()
+            # basket.pop()
+            basket = basket[:-2]
             answer += 2
     
     return answer
