f709f098d47c8321d28d62a813beef4bd8887c7a
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-11-19 12:32:42 +0900

    [level 1] Title: 신고 결과 받기, Time: 1372.97 ms, Memory: 46.6 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,5 +1,5 @@
 def solution(id_list, report, k):
-    answer = []
+    answer = [0] * len(id_list)
     
     # 동일한 유저에 대한 신고 1회로 처리
     report = set(report)
@@ -12,13 +12,10 @@ def solution(id_list, report, k):
             reported[r] = []
         reported[r].append(a)
     
-    # 메일 받을 횟수 저장
-    cnt = {user: 0 for user in id_list}
-    
     # k 이상일 경우 신고자들에게 메일 전송
     for r, a in reported.items():
         if len(a) >= k:
             for j in a:
-                cnt[j] += 1
+                answer[id_list.index(j)] += 1
         
-    return [cnt[user] for user in id_list]
+    return answer
