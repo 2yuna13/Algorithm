1c3b07c48efd21e3e95a13db1c519174c98e4a94
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-11-28 12:16:10 +0900

    [level 2] Title: H-Index, Time: 0.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,11 +1,8 @@
-# 논문 n ,h번 이상 인용된 논문이 h편 이상
-
 def solution(citations):
-    answer = 0
-    citations.sort(reverse=True)
+    citations.sort(reverse = True)
     
     for i in range(len(citations)):
         if citations[i] < i + 1:
             return i
-        
+
     return len(citations)
