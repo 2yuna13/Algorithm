80b041c36d80e6b62f8d9d2f258aa4b87307e6f8
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2024-12-30 09:03:12 +0900

    [level 1] Title: 콜라 문제, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,11 +1,9 @@
 function solution(a, b, n) {
     var answer = 0;
     
-    // 콜라 병 교환 a => b, 가지고 있는 빈병 n
     while (n >= a) {
-        let r = Math.floor(n / a) * b
-        answer += r
-        n = r + n % a
+        answer += parseInt(n / a) * b;
+        n = parseInt(n / a) * b + n % a;
     }
     
     return answer;
