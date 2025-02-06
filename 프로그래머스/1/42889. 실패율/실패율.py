24227a0ca2ec25cab1014aad9a1422c53416fa55
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2025-02-06 11:06:35 +0900

    [level 1] Title: 실패율, Time: 28.95 ms, Memory: 15 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,18 +1,22 @@
 def solution(N, stages):
     answer = []
-    stage_count = []
-    
-    for i in range(1, N + 2):
-        stage_count.append(stages.count(i))
-    
+    stage_count = [0] * (N + 2)
     failure_rates = []
-    for j in range(N):
-        if stage_count[j] > 0:
-            failure_rate = stage_count[j] / sum(stage_count[j:])
-            answer.append((failure_rate, j + 1))
+    
+    for stage in stages:
+        stage_count[stage] += 1
+    
+    for i in range(N):
+        current_players = stage_count[i + 1]
+        remaining_players = sum(stage_count[i + 1:])
+        
+        if current_players > 0:
+            failure_rates.append((current_players / remaining_players, i + 1))
         else:
-            answer.append((0, j + 1))
+            failure_rates.append((0, i + 1))
     
-    answer.sort(key=lambda x: (-x[0], x[1]))
+    failure_rates.sort(key=lambda x: (-x[0], x[1]))
+    for failure_rate in failure_rates:
+        answer.append(failure_rate[1])
     
-    return [stage for failure_rate, stage in answer]
+    return answer
