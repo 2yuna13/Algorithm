69a11da7029c55cfdc941a7f7878771ae4c1c3af
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2025-02-11 11:36:56 +0900

    [level 2] Title: 피로도, Time: 41.67 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,20 +1,22 @@
-from itertools import permutations
+def dfs(k, dungeons, visited, count):
+    global answer
+    answer = max(answer, count)
+        
+    for i in range(len(dungeons)):
+        if not visited[i]:
+            minimum, consume = dungeons[i]
+            
+            if k >= minimum:
+                visited[i] = True
+                dfs(k - consume, dungeons, visited, count + 1)
+                visited[i] = False
+        
 
 def solution(k, dungeons):
+    global answer
     answer = 0
     
-    #모든 던전 탐색
-    for dungeon in permutations(dungeons):
-        fatigue = k
-        cnt = 0
-        
-        for minimum, consume in dungeon:
-            if fatigue >= minimum:
-                fatigue -= consume
-                cnt += 1
-            else:
-                break
-    
-        answer = max(answer, cnt)
+    visited = [False] * len(dungeons)
+    dfs(k, dungeons, visited, 0)
             
     return answer
