fbda897c62cd8153d26d5892869adba5bd038461
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2025-02-13 16:50:52 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,11 +1,9 @@
 def solution(cards1, cards2, goal):
-    idx1, idx2 = 0, 0
-    
     for word in goal:
-        if idx1 < len(cards1) and cards1[idx1] == word:
-            idx1 += 1
-        elif idx2 < len(cards2) and cards2[idx2] == word:
-            idx2 += 1
+        if len(cards1) > 0 and cards1[0] == word:
+            cards1.pop(0)
+        elif len(cards2) > 0 and cards2[0] == word:
+            cards2.pop(0)
         else:
             return "No"
         
