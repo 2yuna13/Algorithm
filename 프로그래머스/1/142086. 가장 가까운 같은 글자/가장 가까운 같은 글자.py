7db977fab6ce04dcc40e6a423faabb2b874aa067
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2025-02-14 15:42:02 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.47 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = []
+    dic = {}
+    
+    for i, v in enumerate(s):
+        if v not in dic.keys():
+            answer.append(-1)
+        else:
+            answer.append(i - dic[v])
+            
+        dic[v] = i
+    
+    return answer
