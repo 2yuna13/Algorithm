6a629dee4ce7579dce472b4d0865b016873ca87f
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2025-02-25 11:43:19 +0900

    [level 1] Title: 문자열 나누기, Time: 3.55 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,16 +1,21 @@
 def solution(s):
     answer = 0
     cnt_x, cnt_notx = 0, 0
-    x = ""
+    x = s[0]
     
-    for i in s:
-        if cnt_x == cnt_notx:
-            x = i
-            cnt_x += 1
-            answer += 1
-        elif x == i:
+    for i in range(len(s)):
+        if s[i] == x:
             cnt_x += 1
         else:
             cnt_notx += 1
+            
+        if cnt_x == cnt_notx:
+            answer += 1
+            cnt_x, cnt_notx = 0, 0
+            if i + 1 < len(s):
+                x = s[i+1]
+    
+    if cnt_x != 0 or cnt_notx != 0:
+        answer += 1
         
     return answer
