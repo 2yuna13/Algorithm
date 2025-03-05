9dc025759d785366197f62769d3eba47bdd1106f
2yuna13 <121540949+2yuna13@users.noreply.github.com>
2025-03-05 18:31:24 +0900

    [level 2] Title: 오픈채팅방, Time: 108.36 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,22 +1,20 @@
 def solution(record):
     answer = []
     user_dict = {}
-    actions = []
     
     for i in record:
         action = i.split()
-        if action[0] == 'Enter':
-            user_dict[action[1]] = action[2]
-            actions.append((action[0], action[1]))
-        elif action[0] == 'Leave':
-            actions.append((action[0], action[1]))
-        elif action[0] == 'Change':
+        
+        if action[0] == 'Enter' or action[0] == 'Change':
             user_dict[action[1]] = action[2]
             
-    for action, user_id in actions:
-        if action == 'Enter':
-            answer.append(f"{user_dict[user_id]}님이 들어왔습니다.")
-        elif action == 'Leave':
-            answer.append(f"{user_dict[user_id]}님이 나갔습니다.")
+    for j in record:
+        action = j.split()
+        nickname = user_dict[action[1]]
+        
+        if action[0] == 'Enter':
+            answer.append(f"{nickname}님이 들어왔습니다.")
+        elif action[0] == 'Leave':
+            answer.append(f"{nickname}님이 나갔습니다.")
     
     return answer
